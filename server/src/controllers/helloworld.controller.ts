ada8cad9efa56ec2e2e290ad8f40cefeae8fb60d
Sushant Pandey <sushhantpandey@gmail.com>
2024-06-13 00:35:24 +0530

    chore: delete controllers directory

--- server/src/controllers/helloworld.controller.ts
+++
@@ -1,8 +0,0 @@
-import type { ExpressRequest, ExpressResponse } from "../config/types";
-
-export function HelloWorldController(
-  req: ExpressRequest,
-  res: ExpressResponse
-) {
-  res.send("<h1>Hello world</h1>");
-}
